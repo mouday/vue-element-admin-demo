57a3162126ec4f44641ddcc781d88e6436234b36
pengshiyu <1940607002@qq.com>
2021-05-10 14:35:22 +0800

    fix

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# build
+npm run build:prod
+
+# deploy
+cp -r ./dist/* ../vue-element-admin-demo-gh-pages
+
+cd ../vue-element-admin-demo-gh-pages
+
+# push
+git add .
+
+git commit -m 'deploy'
+
+git push
+
+cd -
+
+echo 'deploy success'
