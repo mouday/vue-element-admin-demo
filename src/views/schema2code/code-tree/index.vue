1d35035e7b57c430c2a0f12d69f3cea2b554c80b
pengshiyu <1940607002@qq.com>
2021-06-24 10:45:07 +0800

    fix

--- src/views/schema2code/code-tree/index.vue
+++ src/views/schema2code/code-tree/index.vue
@@ -51,41 +51,12 @@ export default {
           label: 'edit.vue',
           children: [
             {
-              label: '二级 2-1',
-              children: [
-                {
-                  label: '三级 2-1-1',
-                },
-              ],
+              label: 'edit.vue',
+              value: 'edit.njk',
             },
             {
-              label: '二级 2-2',
-              children: [
-                {
-                  label: '三级 2-2-1',
-                },
-              ],
-            },
-          ],
-        },
-        {
-          label: '一级 3',
-          children: [
-            {
-              label: '二级 3-1',
-              children: [
-                {
-                  label: '三级 3-1-1',
-                },
-              ],
-            },
-            {
-              label: '二级 3-2',
-              children: [
-                {
-                  label: '三级 3-2-1',
-                },
-              ],
+              label: 'dialog-edit.vue',
+              value: 'dialog-edit.njk',
             },
           ],
         },
